f687b0ece45a52ff18dc425527a7d81b9673775a
cly4auHo <arsen.dmitriev96@gmail.com>
2020-01-27 03:47:07 +0200

    Lesson2

---
+++ hometaskpudge/TypeOfTranslate.java
@@ -0,0 +1,40 @@
+package hometaskpudge;
+
+import java.io.Serializable;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class TypeOfTranslate implements Serializable {
+
+    private transient final String mes = "MessageBundle";
+    private transient final String defaultLanuage = "en";
+    private transient final String defaultCountry = "EN";
+
+    private transient ResourceBundle bundle;
+    private transient Locale locale;
+    private String language;
+    private String country;
+
+    public TypeOfTranslate() {     
+    }
+
+    public void setLanguage(String s) {
+        language = s.toLowerCase();
+        country = s.toUpperCase();
+
+        locale = new Locale(language, country);
+        bundle = ResourceBundle.getBundle(mes, locale);
+    }
+
+    public String getLanguage() {
+        return language == null ? defaultLanuage : language;
+    }
+
+    public String getString(String key) {
+        try {
+            return bundle.getString(key);
+        } catch (Exception ex) {
+            return null;
+        }
+    }
+}
